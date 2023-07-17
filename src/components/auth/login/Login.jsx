4feebd9c8774e02279cde1e7f2fd35f947496ae7
mmaruna <mmaruna@croz.hr>
2023-07-17 09:02:07 +0200

    Fx

--- src/components/auth/login/Login.jsx
+++ src/components/auth/login/Login.jsx
@@ -1,22 +1,25 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {Button, Divider, Grid, TextField, Typography} from "@mui/material";
 import {LoginRounded} from "@mui/icons-material";
 import {useNavigate} from "react-router-dom";
+import {login} from "../../../functions/auth/Auth";
 
 const Login = () => {
     const navigate = useNavigate();
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
 
     return (
         <>
             <Grid direction="column" justifyContent="center" justifyItems="center" spacing={2} marginX={5}>
                 <Grid item margin={2}>
-                    <TextField label="username"/>
+                    <TextField label="username" onChange={(e) => {setUsername(e.target.value)}}/>
                 </Grid>
                 <Grid item margin={2}>
-                    <TextField label="password"/>
+                    <TextField label="password" onChange={(e) => {setPassword(e.target.value)}}/>
                 </Grid>
                 <Grid item margin={2} textAlign="center">
-                    <Button variant="contained">
+                    <Button variant="contained" onClick={() => {login(username, password).then(r => console.log(r))}}>
                         Login  <LoginRounded />
                     </Button>
                 </Grid>
