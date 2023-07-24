141850704682f794098330f3d698058118b41562
mmaruna <mmaruna@croz.hr>
2023-07-24 18:07:04 +0200

    Added profile data update page

--- src/components/profile/UpdateProfile.jsx
+++ src/components/profile/UpdateProfile.jsx
@@ -1,9 +1,14 @@
 import React, {useContext} from 'react'
 import {Avatar, Box, Button, Card, TextField, Typography} from "@mui/material";
 import {UserContext} from "../../App";
+import {useCookies} from "react-cookie";
+import {getSelf, updateSelf} from "../../functions/user/User";
+import {useNavigate} from "react-router-dom";
 
 export const UpdateProfile = () => {
     const { user, setUser } = useContext(UserContext);
+    const [ cookie, setCookie, removeCookie ] = useCookies(['access_token']);
+    const navigate = useNavigate();
     const [name, setName] = React.useState('');
     const [surname, setSurname] = React.useState('');
     const [email, setEmail] = React.useState('');
@@ -14,6 +19,24 @@ export const UpdateProfile = () => {
     const [phone, setPhone] = React.useState('');
     const [aboutMe, setAboutMe] = React.useState('');
 
+    const callUpdate = async () => {
+        const response = await updateSelf(cookie.access_token, name, surname, email, phone, address, city, zipCode, country, aboutMe);
+
+        if (response.status === 202) {
+            await getSelf(cookie.access_token)
+                .then((response) => {
+                    setUser(response.data);
+
+                    navigate('/profile');
+                })
+                .catch((error) => {
+                    console.log(error);
+                });
+        } else {
+            console.log(response);
+        }
+    };
+
     return (
         <>
             <Card sx={{my: 1.5}}>
@@ -22,41 +45,41 @@ export const UpdateProfile = () => {
                         <Avatar sx={{ width: 136, height: 136 }} src="/static/images/avatar/2.jpg" alt="Avatar"/>
                     </Box>
                     <Box sx={{display: {md: 'flex'}, flexDirection: {md: 'column'}, height: {md: 'max'}, mt: 4, ml: {xs: 2}, mx: 'auto'}}>
-                        <Typography variant="h6" component="h6">
-                            Name: <TextField label="name" onChange={(e) => {setName(e.target.value)}}/>
+                        <Typography variant="h6" component="h6" sx={{display: "flex", alignItems: "center", my: 1}}>
+                            Name:  <TextField label="name" onChange={(e) => {setName(e.target.value)}}/>
                         </Typography>
-                        <Typography variant="h6" component="h6">
+                        <Typography variant="h6" component="h6" sx={{display: "flex", alignItems: "center", my: 1}}>
                             Surname: <TextField label="surname" onChange={(e) => {setSurname(e.target.value)}}/>
                         </Typography>
-                        <Typography variant="h6" component="h6">
+                        <Typography variant="h6" component="h6" sx={{display: "flex", alignItems: "center", my: 1}}>
                             Email: <TextField label="email" onChange={(e) => {setEmail(e.target.value)}}/>
                         </Typography>
-                        <Typography variant="h6" component="h6">
+                        <Typography variant="h6" component="h6" sx={{display: "flex", alignItems: "center", my: 1}}>
                             Address: <TextField label="address" onChange={(e) => {setAddress(e.target.value)}}/>
                         </Typography>
                     </Box>
                     <Box sx={{display: {md: 'flex'}, flexDirection: {md: 'column'}, height: {md: 'max'}, mt: {md: 4}, ml: {xs: 2}, mx: 'auto'}}>
-                        <Typography variant="h6" component="h6">
+                        <Typography variant="h6" component="h6" sx={{display: "flex", alignItems: "center", my: 1}}>
                             City: <TextField label="city" onChange={(e) => {setCity(e.target.value)}}/>
                         </Typography>
-                        <Typography variant="h6" component="h6">
+                        <Typography variant="h6" component="h6" sx={{display: "flex", alignItems: "center", my: 1}}>
                             Zip code: <TextField label="zip code" onChange={(e) => {setZipCode(e.target.value)}}/>
                         </Typography>
-                        <Typography variant="h6" component="h6">
+                        <Typography variant="h6" component="h6" sx={{display: "flex", alignItems: "center", my: 1}}>
                             Country: <TextField label="country" onChange={(e) => {setCountry(e.target.value)}}/>
                         </Typography>
-                        <Typography variant="h6" component="h6">
+                        <Typography variant="h6" component="h6" sx={{display: "flex", alignItems: "center", my: 1}}>
                             Phone: <TextField label="phone" onChange={(e) => {setPhone(e.target.value)}}/>
                         </Typography>
                     </Box>
                 </Box>
                 <Box>
-                    <Typography variant="h6" component="h6" sx={{m: 2}}>
+                    <Typography variant="h6" component="h6" sx={{display: "flex", alignItems: "center", m: 2}}>
                         About me: <TextField label="aboutMe" onChange={(e) => {setAboutMe(e.target.value)}}/>
                     </Typography>
                 </Box>
                 <Box>
-                    <Button variant="contained" sx={{m: 'auto', display: 'flex', mb: 2}}>
+                    <Button variant="contained" sx={{m: 'auto', display: 'flex', mb: 2}} onClick={() => {callUpdate()}}>
                         Update
                     </Button>
                 </Box>
--- src/functions/user/User.js
+++ src/functions/user/User.js
@@ -11,3 +11,26 @@ export const getSelf = async (token) => {
         return error;
     });
 }
+
+export const updateSelf = async (token, firstName, lastName, email, phone, address, city, zip, country, aboutMe) => {
+    return await API.put("/user/profile/update", {
+        firstName: firstName,
+        lastName: lastName,
+        email: email,
+        phoneNumber: phone,
+        address: address,
+        city: city,
+        zipCode: zip,
+        country: country,
+        aboutMe: aboutMe
+    }, {
+        headers: {
+            "Authorization": "Bearer " + token,
+            "Content-Type": "application/json"
+        }
+    }).then((response) => {
+        return response;
+    }).catch((error) => {
+        return error;
+    });
+}
