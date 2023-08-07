054417559c97de9aa943638bbeeb4a22da11f13f
mmaruna <mmaruna@croz.hr>
2023-08-07 13:02:51 +0200

    Created admin panel

--- src/components/dialogs/AdminDialog.jsx
+++ src/components/dialogs/AdminDialog.jsx
@@ -1,12 +1,52 @@
 import {useCookies} from "react-cookie";
 import {Box, Button, Dialog, DialogTitle, Typography} from "@mui/material";
 import React, {useEffect, useState} from "react";
-import {getUserById} from "../../functions/admin/admin";
+import {demoteUser, disableUser, enableUser, getUserById, promoteUser} from "../../functions/admin/admin";
 
 const AdminDialog = (props) => {
     const [ cookie, setCookie, removeCookie ] = useCookies(['access_token']);
     const { onClose, userId, open } = props;
     const [user, setUser] = useState([]);
+
+    const Close = () => {
+        onClose();
+    }
+
+    const enable = () => {
+        enableUser(cookie.access_token, userId).then((r) => {
+            getUserById(cookie.access_token, userId).then((r) => {
+                setUser(r) })
+        }).catch((e) => {
+            console.log(e)
+        })
+    }
+
+    const disable = () => {
+        disableUser(cookie.access_token, userId).then((r) => {
+            getUserById(cookie.access_token, userId).then((r) => {
+                setUser(r) })
+        }).catch((e) => {
+            console.log(e)
+        })
+    }
+
+    const promote = () => {
+        promoteUser(cookie.access_token, userId).then((r) => {
+            getUserById(cookie.access_token, userId).then((r) => {
+                setUser(r) })
+        }).catch((e) => {
+            console.log(e)
+        })
+    }
+
+    const demote = () => {
+        demoteUser(cookie.access_token, userId).then((r) => {
+            getUserById(cookie.access_token, userId).then((r) => {
+                setUser(r) })
+        }).catch((e) => {
+            console.log(e)
+        })
+    }
 
     useEffect(() => {
         if(open) {
@@ -16,7 +56,7 @@ const AdminDialog = (props) => {
                 console.log(e)
             })
         }
-    }, [])
+    }, [open, userId])
 
     return(
         <Dialog open={open}>
@@ -25,17 +65,36 @@ const AdminDialog = (props) => {
                 <Box m={2}>
                     <Typography mx={1}> Username: {user.username} </Typography>
                     <Typography mx={1}> Full name: {user.firstName} {user.lastName} </Typography>
+                    <Typography mx={1}> Email: {user.email === null ? "Not set" : user.email}</Typography>
+                    <Typography mx={1}> Phone: {user.phoneNumber === null ? "Not set" : user.phoneNumber}</Typography>
                     <Typography mx={1}> Role: {user.role} </Typography>
+                    <Typography mx={1}> Enabled: {user.active}</Typography>
                 </Box>
                 <Box m={2} sx={{display: 'flex', flexDirection: "column"}}>
-                    <Button sx={{mb: 1}} variant="contained">Promote</Button>
-                    <Button sx={{mb: 1}} variant="contained">Demote</Button>
-                    <Button sx={{mb: 1}} variant="contained">Enable</Button>
-                    <Button sx={{mb: 1}} variant="contained">Disable</Button>
+                    {user.role === "ADMIN" ?
+                        <Button sx={{mb: 1}} variant="contained" disabled={true}>Promote</Button>
+                        :
+                        <Button sx={{mb: 1}} variant="contained" onClick={() => {promote()}}>Promote</Button>
+                    }
+                    {user.role === "STUDENT" ?
+                        <Button sx={{mb: 1}} variant="contained" disabled={true}>Demote</Button>
+                        :
+                        <Button sx={{mb: 1}} variant="contained" onClick={() => {demote()}}>Demote</Button>
+                    }
+                    {user.active === "true" ?
+                        <Button sx={{mb: 1}} variant="contained" disabled={true}>Enable</Button>
+                        :
+                        <Button sx={{mb: 1}} variant="contained" onClick={() => {enable()}}>Enable</Button>
+                    }
+                    {user.active === "false" ?
+                        <Button sx={{mb: 1}} variant="contained" disabled={true}>Disable</Button>
+                        :
+                        <Button sx={{mb: 1}} variant="contained" onClick={() => {disable()}}>Disable</Button>
+                    }
                     <Button sx={{mb: 1}} variant="contained" color={"warning"}>Delete</Button>
                 </Box>
             </Box>
-            <Button variant="contained" m={2}>
+            <Button variant="contained" sx={{m: 2}} onClick={() => {Close()}}>
                 Close
             </Button>
         </Dialog>
--- src/functions/admin/admin.js
+++ src/functions/admin/admin.js
@@ -27,3 +27,74 @@ export const getUserById = async ( cookie, id) => {
             console.log(e)
         })
 }
+
+export const deleteUser = async (cookie, id) => {
+    return await Api.delete("/admin/user/" + id, {
+        headers: {
+            Authorization: `Bearer ${cookie}`
+        }
+    }).then((r) => {
+        if(r.status === 200) {
+            return r.data
+        }
+    }).catch((e) => {
+        console.log(e)
+    })
+}
+
+export const enableUser = async (cookie, id) => {
+    return await Api.put("/admin/user/" + id + "/enable", {},{
+        headers: {
+            Authorization: `Bearer ${cookie}`
+        }
+    }).then((r) => {
+        if(r.status === 200) {
+            return r.data
+        }
+    }).catch((e) => {
+        console.log(e)
+    })
+}
+
+export const disableUser = async (cookie, id) => {
+    return await Api.put("/admin/user/" + id + "/disable", {},{
+        headers: {
+            Authorization: `Bearer ${cookie}`
+        }
+    }).then((r) => {
+        if(r.status === 200) {
+            return r.data
+        }
+    }).catch((e) => {
+        console.log(e)
+    })
+}
+
+export const promoteUser = async (cookie, id) => {
+    return await Api.put("/admin/user/" + id + "/promote", {},{
+        headers: {
+            Authorization: `Bearer ${cookie}`
+        }
+    }).then((r) => {
+        if(r.status === 200) {
+            return r.data
+        }
+    }).catch((e) => {
+        console.log(e)
+    })
+}
+
+export const demoteUser = async (cookie, id) => {
+    return await Api.put("/admin/user/" + id + "/demote", {},{
+        headers: {
+            Authorization: `Bearer ${cookie}`
+        }
+    }).then((r) => {
+        if(r.status === 200) {
+            return r.data
+        }
+    }).catch((e) => {
+        console.log(e)
+    })
+}
+
